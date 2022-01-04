113e0a45d331d7a951ff55c3331a33c4b8ccd0e1
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-03 21:48:22 -0300

    auth route added to public route

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomeScreen from '../pages/public/HomeScreen';
 import NosotroScreen from '../pages/public/NosotroScreen';
+import AuthRoute from './AuthRoute';
 
 const PublicRoute = () => {
  return (
@@ -10,6 +11,7 @@ const PublicRoute = () => {
    <div>
      <Routes>
        <Route path="nosotros" element={<NosotroScreen />} />
+       <Route path="auth/*" element={<AuthRoute />} />
 
        <Route path="/" element={<HomeScreen />} />
      </Routes>
