9fec27386071a642a552c3295c31699ee5d51284
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-03 21:42:34 -0300

    private routes config

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import HomeBackoffice from '../pages/backoffice/HomeBackoffice';
+import UsersBackoffice from '../pages/backoffice/UsersBackoffice';
 
 const Privateroute = () => {
  return (
@@ -7,8 +9,8 @@ const Privateroute = () => {
       {/* //espacio para el navbar privado */}
       <div>
         <Routes>
-          {/* <Route path="/users" element={< />} />
-          <Route path="/" element={< />} /> */}
+          <Route path="/users" element={<UsersBackoffice />} />
+          <Route path="/" element={<HomeBackoffice />} />
         </Routes>
       </div>
     </>
