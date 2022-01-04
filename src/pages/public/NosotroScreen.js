d4ff860a4f5d817589b52e43e25b6887b1a0b2a5
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-03 21:39:34 -0300

    public route config

---
+++ src/pages/public/NosotroScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NosotroScreen = () => {
+ return (
+  <div>
+    nosotros publico
+  </div>
+ );
+}
+
+export default NosotroScreen;
--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import HomeScreen from '../pages/public/HomeScreen';
+import NosotroScreen from '../pages/public/NosotroScreen';
 
 const PublicRoute = () => {
  return (
@@ -7,9 +9,9 @@ const PublicRoute = () => {
      {/* espacio para el navbar publico */}
    <div>
      <Routes>
-       {/* <Route path="nosotros" element={< />} />
+       <Route path="nosotros" element={<NosotroScreen />} />
 
-       <Route path="/" element={< />} /> */}
+       <Route path="/" element={<HomeScreen />} />
      </Routes>
    </div>
  </>
