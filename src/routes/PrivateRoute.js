8386871438c0eeaeb0f2033ef25daeb25bbbce13
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-03 21:30:09 -0300

    provate route added

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+
+const Privateroute = () => {
+ return (
+  <>
+      {/* //espacio para el navbar privado */}
+      <div>
+        <Routes>
+          {/* <Route path="/users" element={< />} />
+          <Route path="/" element={< />} /> */}
+        </Routes>
+      </div>
+    </>
+ );
+}
+
+export default Privateroute;
