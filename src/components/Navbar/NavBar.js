e854814b32d75ef78735c453c76251579e7eef82
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-08 17:20:46 -0300

    login incorporated and functional

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,24 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import MailIcon from '@mui/icons-material/Mail';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import { Badge } from '@mui/material';
-
-const settings = ['Backoffice', 'Logout'];
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MailIcon from "@mui/icons-material/Mail";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import { Badge, Button, ButtonGroup } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {setLogout} from "../../redux/reducers/authReducer";
+const settings = ["Backoffice", "Logout"];
 const NavBar = () => {
+  const { logged, user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -27,49 +31,56 @@ const NavBar = () => {
     setAnchorElUser(null);
   };
 
+  const handleClick = (e) =>{
+      if("Logout"){
+         dispatch(setLogout());
+      }
+  }
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-        
-          <MailIcon sx={{display:{xs:"none", md:"flex"}}}/>
+          <MailIcon sx={{ display: { xs: "none", md: "flex" } }} />
           <Typography
             variant="h6"
             noWrap
             component="div"
             ml={1}
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
             hekto@gmail.com
           </Typography>
-          <LocalPhoneIcon  sx={{ml:3, display:{xs:"none", md:"flex"}}} />
+          <LocalPhoneIcon sx={{ ml: 3, display: { xs: "none", md: "flex" } }} />
           <Typography
             variant="h6"
             noWrap
             component="div"
             ml={1}
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
             11 3423 2342
           </Typography>
 
-          
           <Typography
             variant="h7"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
             hekto@gmail.com
           </Typography>
 
-        
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {}
           </Box>
 
           <Box mr={2} sx={{ flexGrow: 0 }}>
-            <IconButton size="large" aria-label="show 4 new favorites" color="inherit">
+            <IconButton
+              size="large"
+              aria-label="show 4 new favorites"
+              color="inherit"
+            >
               <Badge badgeContent={4} color="error">
                 <FavoriteIcon />
               </Badge>
@@ -85,47 +96,52 @@ const NavBar = () => {
             </IconButton>
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          {logged ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={user.name} src={user.img} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={(e)=>handleClick(e)}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : (
+            <ButtonGroup  variant="text">
+              <Link to="/auth/login" style={{textDecoration:"none", color:"white"}}>
+                <Button color="inherit">Login</Button>
+              </Link> 
+              <Button
+                color="inherit"
+                sx={{ ml: 3, display: { xs: "none", md: "flex" } }}
+              >
+                Register
+              </Button>
+            </ButtonGroup>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
   );
 };
 export default NavBar;
-
-
-// const pages = [
-//   "hekto@gmail.com",
-//   "11 3423 2342",
-//   "Registro",
-//   "Login",
-//   "Lista deseos",
-// ];
