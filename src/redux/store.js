1105040e2ffc9699ea4099523741289d2b9bc331
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-09 21:43:11 -0300

    categories incorporated to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
+import categorieReducer from "./reducers/categorieReducer";
 
 //reducers
 
@@ -7,6 +8,7 @@ import authReducer from "./reducers/authReducer";
 export default configureStore({
   reducer: {
    auth: authReducer,
+   categories:categorieReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
 });
