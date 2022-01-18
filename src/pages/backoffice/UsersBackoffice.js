d2063e8d2dfaa22ebb2622ccc572cce601d384ed
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-17 23:22:57 -0300

    title users backoffice added

--- src/pages/backoffice/UsersBackoffice.js
+++ src/pages/backoffice/UsersBackoffice.js
@@ -1,56 +1,60 @@
 import React from "react";
-import Container from '@mui/material/Container';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Container from "@mui/material/Container";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import { useSelector } from "react-redux";
-import { Button, ButtonGroup } from "@mui/material";
-
+import { Button, ButtonGroup, Typography } from "@mui/material";
 
 const UsersBackoffice = () => {
-  const {usuariosList, loading} = useSelector(state => state.users);
+  const { usuariosList, loading } = useSelector((state) => state.users);
   return (
     <Container maxWidth="sx">
-         <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Role</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+ 
+      <Typography variant="h4" align="center" mt={5} mb={5}>
+        Usuarios
+      </Typography>  
 
-          { !loading
-             ? usuariosList.map((row) => (
-                  <TableRow
-                    key={row.uid}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.email}</TableCell>
-                    <TableCell align="right">{row.role}</TableCell>
-                    <TableCell align="right">
-                      <ButtonGroup size="small" aria-label="small button group">
-                        <Button color="warning">Edit</Button>
-                        <Button color="error">Delete</Button>
-                      </ButtonGroup>
-                    </TableCell>
-                  </TableRow>
-                ))
-             : <div>generando lista..</div>  
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Role</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!loading ? (
+              usuariosList.map((row) => (
+                <TableRow
+                  key={row.uid}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right">{row.role}</TableCell>
+                  <TableCell align="right">
+                    <ButtonGroup size="small" aria-label="small button group">
+                      <Button color="warning">Edit</Button>
+                      <Button color="error">Delete</Button>
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <div>generando lista..</div>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
