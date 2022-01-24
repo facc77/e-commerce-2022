56837d45347228675fdce4ff41199079bba03f55
facc77 <ferrerfacundo96@gmail.com>
2022-01-24 14:42:52 -0300

    commented code deleted

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -2,19 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Box, Typography } from "@mui/material";
 import ProductCard from "./Card";
-import { Link } from "react-router-dom";
-import MenuItem from "@mui/material/MenuItem";
-import { useDispatch } from "react-redux";
-import { setActiveProduct } from "../redux/reducers/productsReducer";
 
 const CardContainer = () => {
   const { productsList } = useSelector((state) => state.products);
   let lastElements = productsList.slice(-3);
-
-  const dispatch = useDispatch();
-  const callDispatch = (name) => {
-    dispatch(setActiveProduct(name));
-  };
 
   return (
     <>
@@ -40,20 +31,7 @@ const CardContainer = () => {
         }}
       >
         {lastElements.map((product, id) => (
-          <MenuItem
-            key={id}
-            component={Link}
-            to="/detalleProducto"
-            onClick={() => callDispatch(product.name)}
-          >
-            <ProductCard
-              key={id}
-              name={product.name}
-              image={product.img || null}
-              price={product.price}
-              showIcons={true}
-            />
-          </MenuItem>
+          <ProductCard key={id} product={product} />
         ))}
       </Box>
     </>
