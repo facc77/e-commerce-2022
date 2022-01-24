a4691b15e8721143e7941e2f8c1f09d726d45493
facc77 <ferrerfacundo96@gmail.com>
2022-01-24 14:40:50 -0300

    import of catContainer changed

--- src/pages/public/HomeScreen.js
+++ src/pages/public/HomeScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import CarouselHome from "../../components/Carousel/CarouselHome";
 import CardContainer from "../../components/CardContainer";
 import ServicesContainer from "../../components/ServicesContainer";
-import CatContainer from "../../components/CatContainer";
+import CatContainer from "../../components/Categories/CatContainer";
 
 const HomeScreen = () => {
   return (
