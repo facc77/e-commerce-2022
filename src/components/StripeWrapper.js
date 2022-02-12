f725a22cf8a8a44798a4644fc85355f7e75982a9
facc77 <ferrerfacundo96@gmail.com>
2022-02-12 12:25:17 -0300

    fixing stripe live problem

--- src/components/StripeWrapper.js
+++ src/components/StripeWrapper.js
@@ -5,7 +5,9 @@ import { loadStripe } from "@stripe/stripe-js";
 
 //used backtics to solve :Invalid value for Stripe(): apiKey should be a string.
 const stripeKey = `${process.env.REACT_APP_STRIPE_KEY}`;
-const stripePromise = loadStripe(stripeKey);
+const stripePromise = loadStripe(
+  "pk_test_51KMGSvD2oePE5Ls1FOKrZWSCMC57ahMvUc7rgw4g7i1z25hS8aLUo0VrKbSlp6otfISbc9PH3YKLvFjfF51fgBxe00GLeFCgIs"
+);
 
 const StripeWrapper = () => {
   return (
